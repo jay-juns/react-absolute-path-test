3e15704d48d7fa6177660f9ae78c360d0c25c6ed
jay-juns <necrozerg2@naver.com>
2022-08-24 10:12:48 +0900

    test

---
+++ src/component/test/test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Dev from 'dev/dev'
+
+
+function test() {
+  return (
+    <div>
+        test
+        <Dev />
+    </div>
+  )
+}
+
+export default test
---
+++ src/dev/dev.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function dev() {
+  return (
+    <div>여기도 테스트</div>
+  )
+}
+
+export default dev
