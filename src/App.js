97396d5d8b6caddf4f45c0e22a2e0da7ac55381b
jay-juns <necrozerg2@naver.com>
2022-08-24 10:39:48 +0900

    image test

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import Test from 'component/test/test'
+import 'style/style.css'
 
 function App() {
   return (
     <div className="App">
       <Test />
       테스트2
+      <div className="div"></div>
     </div>
   );
 }
 
-export default App;
+export default App; 
