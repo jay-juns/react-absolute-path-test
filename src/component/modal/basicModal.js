802b42df7aa7163bfee98d03d0393eaea5eb015b
jay-juns <necrozerg2@naver.com>
2022-08-26 14:04:37 +0900

    외부영역 클릭시 닫기

---
+++ src/component/modal/basicModal.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function basicModal() {
+    
+    return (
+        <div>
+            모달
+        </div>
+    )
+}
+
+export default basicModal
--- src/component/test/test.js
+++ src/component/test/test.js
@@ -1,14 +1,38 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Dev from 'dev/dev'
 
 
-function test() {
+function Test() {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleOption = () => setIsOpen((isOpen) => !isOpen)
+  const ref = useRef(null);
+
+  const handleClickOutSide = (e) => {
+    console.log(ref.current.contains(e.target))
+    if (isOpen && !ref.current.contains(e.target)) {
+      setIsOpen(false)
+    }
+  }
+
+  useEffect(() => {
+    if (isOpen) document.addEventListener('mousedown', handleClickOutSide)
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutSide)
+    }
+  },[])
+
   return (
     <div>
-        test
+        <button className='btn' onClick={toggleOption}>클릭</button>
+        {isOpen ? (
+          <div ref={ref}>
+            open?
+          </div>
+        ) : ''}
         <Dev />
+        <button className='btn'>다른 btn</button>
     </div>
   )
 }
 
-export default test
+export default Test
